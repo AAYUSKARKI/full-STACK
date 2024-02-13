340fb735b84f8d50f9ea922ca3ed917aaa213556
AAYUS KARKI <alexkarki2060@gmail.com>
2024-02-13 17:02:12 +0545

    connected fend with bend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [jokes,setjokes]= useState([])
+  useEffect(() => {
+  axios.get(`/api/jokes`)
+  .then((response)=>{
+    setjokes(response.data)
+    console.log(response.data);
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+  })
+  
+
   return (
     <>
     <h1>MY FULL STACK PROJECT</h1>
       <p>Jokes : {jokes.length}</p>
       {
-        jokes.map((joke, index)=>{
+        jokes.map((joke, index)=>(
           <div key={joke.id}>
             <h2>{joke.title}</h2>
+            <p>{joke.content}</p>
             </div>
-        })
+        ))
       }
     </>
   )
